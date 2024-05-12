8ead1b18723ad160288503cf3f93750b58957375
Nursultan Kadyrov <nur@roky.rocks>
2024-05-12 13:16:19 +0500

    feat: make Layout component

---
+++ src/layout/LayoutMain.tsx
@@ -0,0 +1,34 @@
+import { ReactNode, useState } from "react";
+
+import classNames from "classnames";
+
+import { OverviewIcon } from "../components/icons/OverviewIcon";
+import styles from "./LayoutMain.module.scss";
+
+type Props = {
+  children: ReactNode;
+};
+
+// #8F92B5
+const LayoutMain = ({ children }: Props) => {
+  const [isAsideOpen, setAsideOpen] = useState(false);
+
+  const toggleAside = () => setAsideOpen(!isAsideOpen);
+
+  return (
+    <section className={styles.layoutMain}>
+      <aside
+        className={classNames(styles.aside, { [styles.open]: isAsideOpen })}
+      >
+        <button onClick={toggleAside}>
+          {isAsideOpen ? "Скрыть" : "Показать"}
+        </button>
+
+        <OverviewIcon color="#151422"></OverviewIcon>
+      </aside>
+      <div className={styles.content}>{children}</div>
+    </section>
+  );
+};
+
+export default LayoutMain;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import LayoutMain from "./layout/LayoutMain.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <LayoutMain>
+      <App />
+    </LayoutMain>
+  </React.StrictMode>
+);
