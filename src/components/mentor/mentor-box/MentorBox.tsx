63c0256a5eebe09ba6aecf05b63a771313602eb8
Nursultan Kadyrov <nur@roky.rocks>
2024-05-16 17:50:50 +0500

    feat: make tree module

--- src/components/mentor/mentor-box/MentorBox.tsx
+++ src/components/mentor/mentor-box/MentorBox.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./MentorBox.scss";
 import MentorCard from "../mentor-card/MentorCard";
+import { Mentor } from "@/api/mentor-rest/types";
 
-type Props = {};
+type Props = {
+  mentor: Mentor;
+};
 
-export const MentorBox = (props: Props) => {
+export const MentorBox = ({ mentor }: Props) => {
   return (
     <div className="mentor-box">
       <h3>MentorCard</h3>
-      <MentorCard />
+      <MentorCard mentor={mentor} />
     </div>
   );
 };
--- src/pages/CoursePage/CoursePage.tsx
+++ src/pages/CoursePage/CoursePage.tsx
@@ -24,6 +24,25 @@ type Props = {
   title?: string;
 };
 
+const mentorMock = {
+  id: 1,
+  specialties: [
+    {
+      id: 1,
+      title: "Profile1",
+    },
+    {
+      id: 2,
+      title: "Profile2",
+    },
+  ],
+  first_name: "DAUREN",
+  last_name: "KARASSAYEV",
+  biography: "DASDSAD",
+  image:
+    "/images/%D0%91%D0%B5%D0%B7_%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F.png",
+};
+
 const CoursePage = (props: Props) => {
   const { img, title } = props;
 
@@ -119,7 +138,7 @@ const CoursePage = (props: Props) => {
           </div>
         </div>
         <div className={styles.content__right}>
-          <MentorBox />
+          <MentorBox mentor={mentorMock} />
         </div>
       </div>
     </div>
